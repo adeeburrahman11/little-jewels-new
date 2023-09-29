76a405c1c120abed7167ce61c58d8f5286f73aa9
harshalhonde21 <harshalhonde17@gmail.com>
2023-09-29 23:31:16 +0530

    readytosucss

--- src/components/pages/Home/HomeAbout.jsx
+++ src/components/pages/Home/HomeAbout.jsx
@@ -1,12 +1,33 @@
-import React from "react";
 import "./HomeAbout.css";
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+AOS.init({
+    disable: false, 
+  startEvent: 'DOMContentLoaded', // name of the event dispatched on the document, that AOS should initialize on
+  initClassName: 'aos-init', // class applied after initialization
+  animatedClassName: 'aos-animate', // class applied on animation
+  useClassNames: false, // if true, will add content of `data-aos` as classes on scroll
+  disableMutationObserver: false, // disables automatic mutations' detections (advanced)
+  debounceDelay: 50, // the delay on debounce used while resizing window (advanced)
+  throttleDelay: 99, // the delay on throttle used while scrolling the page (advanced)
+  
+
+  // Settings that can be overridden on per-element basis, by `data-aos-*` attributes:
+  offset: 120, // offset (in px) from the original trigger point
+  delay: 100, // values from 0 to 3000, with step 50ms
+  duration: 500, // values from 0 to 3000, with step 50ms
+  easing: 'ease', // default easing for AOS animations
+  once: false, // whether animation should happen only once - while scrolling down
+  mirror: false, // whether elements should animate out while scrolling past them
+  anchorPlacement: 'top-bottom',
+});
 
 export const HomeAbout = () => {
   return (
     <div className="about-main">
       <h1 className="about-heading">About Us</h1>
-      <div className="about-container">
-        <div className="image-container">
+      <div className="about-container" data-aos="slide-right" data-aos-once="false" >
+        <div className="image-container" >
           <img src="/src/assets/img/pexels-rdne-stock-project-8363102.jpg" />
         </div>
         <div className="text-container">
@@ -20,7 +41,7 @@ export const HomeAbout = () => {
           </p>
         </div>
       </div>
-      <div className="about-container1">
+      <div className="about-container1" data-aos="slide-left" data-aos-once="false">
         <div className="image-container1">
           <img src="/src/assets/img/pexels-rdne-stock-project-8363102.jpg" />
         </div>
--- src/components/pages/Home/Testimonals.jsx
+++ src/components/pages/Home/Testimonals.jsx
@@ -1,5 +1,26 @@
-import React from "react";
-import "./Testimonals.css"; // You can create a separate CSS file for styling
+import "./Testimonals.css";
+
+import AOS from "aos";
+import "aos/dist/aos.css";
+AOS.init({
+  disable: false,
+  startEvent: "DOMContentLoaded", // name of the event dispatched on the document, that AOS should initialize on
+  initClassName: "aos-init", // class applied after initialization
+  animatedClassName: "aos-animate", // class applied on animation
+  useClassNames: false, // if true, will add content of `data-aos` as classes on scroll
+  disableMutationObserver: false, // disables automatic mutations' detections (advanced)
+  debounceDelay: 50, // the delay on debounce used while resizing window (advanced)
+  throttleDelay: 99, // the delay on throttle used while scrolling the page (advanced)
+
+  // Settings that can be overridden on per-element basis, by `data-aos-*` attributes:
+  offset: 120, // offset (in px) from the original trigger point
+  delay: 100, // values from 0 to 3000, with step 50ms
+  duration: 500, // values from 0 to 3000, with step 50ms
+  easing: "ease", // default easing for AOS animations
+  once: false, // whether animation should happen only once - while scrolling down
+  mirror: false, // whether elements should animate out while scrolling past them
+  anchorPlacement: "top-bottom",
+});
 
 function Testimonals() {
   return (
@@ -14,7 +35,7 @@ function Testimonals() {
           width="80px"
         />
       </div>
-      <div className="testimonals-wrapper">
+      <div className="testimonals-wrapper" data-aos="slide-right">
         <div className="card-testimonals">
           <p>
             <img
@@ -24,10 +45,16 @@ function Testimonals() {
               height="50px"
               width="50px"
             />
-            I am very happy with my choice of school for Anshika. I feel proud as a father to see the emerging talents in my daughter. I would like to thank all the teachers for their hardwork & constant dedication to keep parents updated & informed during these challenging times. 
-Strong leadership is the foundation of a school's success & without the Sr. Principal, Mrs. Anmol Badjatia Ma'am it would not been possible.                  
-We feel proud & fortunate to have Anmol Ma'am as the Sr. Principal of the school.  Ma’am has balanced the school so well during these rapidly changing times.
-Thanks to "Little Jewels" for providing a great learning system to all the students.
+            I am very happy with my choice of school for Anshika. I feel proud
+            as a father to see the emerging talents in my daughter. I would like
+            to thank all the teachers for their hardwork & constant dedication
+            to keep parents updated & informed during these challenging times.
+            Strong leadership is the foundation of a school's success & without
+            the Sr. Principal, Mrs. Anmol Badjatia Ma'am it would not been
+            possible. We feel proud & fortunate to have Anmol Ma'am as the Sr.
+            Principal of the school. Ma’am has balanced the school so well
+            during these rapidly changing times. Thanks to "Little Jewels" for
+            providing a great learning system to all the students.
           </p>
           <section className="parents-corner">
             <h2 className="parents-review">

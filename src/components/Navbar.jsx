ab987c983fccb47ea4c212007c66756674fb12ef
adeeburrahman11 <adeeburrahman9021@gmail.com>
2023-10-22 16:00:59 +0530

    on click close navbar done

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -32,7 +32,9 @@ function Navbar() {
       </div>
       <ul className={click ? "open" : ""}>
         <li>
-          <NavLink to="/home">Home</NavLink>
+          <NavLink to="/home" onClick={closeMobileMenu}>
+            Home
+          </NavLink>
         </li>
         <li>
           <div
@@ -44,11 +46,21 @@ function Navbar() {
             <i className="fa-solid fa-angle-down" />
             {isAboutDropdownOpen && (
               <div className="dropdown-content">
-                <Link to="/about/vision&mission">Vision & Mission</Link>
-                <Link to="/about/director-message">Director Message</Link>
-                <Link to="/about/management">Management</Link>
-                <Link to="/about/teams">Team Little Jewels</Link>
-                <Link to="/about/testimonials">Testimonials</Link>
+                <Link to="/about/vision&mission" onClick={closeMobileMenu}>
+                  Vision & Mission
+                </Link>
+                <Link to="/about/director-message" onClick={closeMobileMenu}>
+                  Director Message
+                </Link>
+                <Link to="/about/management" onClick={closeMobileMenu}>
+                  Management
+                </Link>
+                <Link to="/about/teams" onClick={closeMobileMenu}>
+                  Team Little Jewels
+                </Link>
+                <Link to="/about/testimonials" onClick={closeMobileMenu}>
+                  Testimonials
+                </Link>
               </div>
             )}
           </div>
@@ -63,22 +75,36 @@ function Navbar() {
             <i className="fa-solid fa-angle-down" />
             {isGalleryDropdownOpen && (
               <div className="dropdown-content">
-                <Link to="/gallery/events">Events</Link>
-                <Link to="/gallery/celebrations">Celebrations</Link>
-                <Link to="/gallery/achievements">Achievements</Link>
-                <Link to="/gallery/media">Media</Link>
+                <Link to="/gallery/events" onClick={closeMobileMenu}>
+                  Events
+                </Link>
+                <Link to="/gallery/celebrations" onClick={closeMobileMenu}>
+                  Celebrations
+                </Link>
+                <Link to="/gallery/achievements" onClick={closeMobileMenu}>
+                  Achievements
+                </Link>
+                <Link to="/gallery/media" onClick={closeMobileMenu}>
+                  Media
+                </Link>
               </div>
             )}
           </div>
         </li>
         <li>
-          <NavLink to="/academics">Academics</NavLink>
+          <NavLink to="/academics" onClick={closeMobileMenu}>
+            Academics
+          </NavLink>
         </li>
         <li>
-          <NavLink to="/contact">Careers</NavLink>
+          <NavLink to="/contact" onClick={closeMobileMenu}>
+            Careers
+          </NavLink>
         </li>
         <li>
-          <NavLink to="/badjate-group">Badjate Group</NavLink>
+          <NavLink to="/badjate-group" onClick={closeMobileMenu}>
+            Badjate Group
+          </NavLink>
         </li>
       </ul>
     </nav>

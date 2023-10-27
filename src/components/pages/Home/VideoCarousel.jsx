7e1bfec77621de0eb460d8de34cafa7d5dcea2cc
unknown <dipeshhaldar787@gmail.com>
2023-10-27 20:41:41 +0530

    Video Functionality Added

--- src/components/pages/Home/VideoCarousel.jsx
+++ src/components/pages/Home/VideoCarousel.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Carousel } from "react-responsive-carousel";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
-import "./VideoCarousel.css"; 
+import "./VideoCarousel.css";
 
 const VideoCarousel = () => {
   const videos = [
@@ -13,19 +13,24 @@ const VideoCarousel = () => {
   return (
     <>
       <h1 className="main-heading__videocarousel">Videos</h1>
-    <div className="videocarousel_wrapper">
-      <div className="carousel-container">
-        <Carousel showArrows={false} showThumbs={false} infiniteLoop={true} autoPlay={true}>
-          {videos.map((video, index) => (
-            <div key={index} className="video-slide">
-              <video controls>
-                <source src={video} type="video/mp4" />
-                Your browser does not support the video tag.
-              </video>
-            </div>
-          ))}
-        </Carousel>
-      </div>
+      <div className="videocarousel_wrapper">
+        <div className="carousel-container">
+          <Carousel
+            showArrows={false}
+            showThumbs={false}
+            infiniteLoop={true}
+            autoPlay={true}
+          >
+            {videos.map((video, index) => (
+              <div key={index} className="video-slide">
+                <video controls>
+                  <source src={video} type="video/mp4" />
+                  Your browser does not support the video tag.
+                </video>
+              </div>
+            ))}
+          </Carousel>
+        </div>
       </div>
     </>
   );

a0947a81a17da4230c96f545c56518b6142b923b
harshalhonde21 <harshalhonde17@gmail.com>
2023-10-29 18:00:57 +0530

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ import "./App.css";
 import SportsAndWellness from "./components/pages/Gallery/SportsAndWellness";
 import ScrollToTop from "./components/ScrollToTop";
 import Admission from "./components/pages/Admission/Admission";
-import { Toaster } from "react-hot-toast"; // for notification don't remover updated HH
+import { Toaster } from "react-hot-toast"; 
 
 function App() {
   return (
